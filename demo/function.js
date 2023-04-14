481843c4a383aec33df8d3a51532133cf32f1102
Aditya_Parmar@dev <105008414+Adityajparmar37@users.noreply.github.com>
2023-04-14 18:53:03 +0530

    Add files via upload

---
+++ demo/function.js
@@ -0,0 +1,6 @@
+function cookMaagi(maagi, pani, pan) {
+
+    console.log(" Your Maggi will be ready in " + maagi * 2 + " minute " + " and ingredients used are : - " + maagi + " Maggi " + pani + " cup of water " + pan + " pan ");
+}
+
+cookMaagi( 6 , 8 , 1);
